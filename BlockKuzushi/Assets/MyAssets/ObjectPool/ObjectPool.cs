a33ebaff3a3a2731b432ede44caaf24ee10a4482
TSITCL\s152106 <shibata.public@gmail.com>
2017-12-12 11:08:41 +0900

    gitignore修正

---
+++ BlockKuzushi/Assets/MyAssets/ObjectPool/ObjectPool.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ObjectPool
+{
+	//<Prefab名,GameObject>
+	static Dictionary<string, List<GameObject>> _reserved = new Dictionary<string, List<GameObject>>();
+
+	static GameObject _poolRoot = null;
+	static Dictionary<string, GameObject> _groupRoots = new Dictionary<string, GameObject>();
+
+	/// <summary>初期化関数</summary>
+	static void Initialize()
+	{
+		_poolRoot = new GameObject("ObjectPool");
+		GameObject.DontDestroyOnLoad(_poolRoot);
+	}
+	/// <summary>初期化チェック関数</summary>
+	static void CheckInitialize()
+	{
+		if (_poolRoot == null)
+			Initialize();
+	}
+
+	/// <summary>インスタンス生成</summary>
+	/// <param name="source">参照元</param>
+	/// <returns>インスタンス</returns>
+	static GameObject CreateInstance(GameObject source)
+	{
+		var instance = GameObject.Instantiate(source);
+		instance.name = source.name + _reserved[source.name].Count;
+		return instance;
+	}
+
+	/// <summary>オブジェクトグループに新規インスタンスを追加</summary>
+	/// <param name="key">キー</param>
+	static void AddObjectGroup(string key)
+	{
+		var objRoot = new GameObject(key);
+		objRoot.transform.SetParent(_poolRoot.transform);
+		_groupRoots.Add(key, objRoot);
+
+		_reserved.Add(key, new List<GameObject>());
+	}
+
+	/// <summary>インスタンスを確保する</summary>
+	/// <param name="prefab">参照元のプレハブ</param>
+	/// <param name="reserveAmount">確保量</param>
+	public static void Reserve(GameObject prefab, int reserveAmount)
+	{
+		CheckInitialize();
+
+		if (!_groupRoots.ContainsKey(prefab.name))
+			AddObjectGroup(prefab.name);
+
+		for(int i=0;i<reserveAmount;i++)
+		{
+			var obj = CreateInstance(prefab);
+			obj.SetActive(false);
+			obj.transform.SetParent(_groupRoots[prefab.name].transform);
+			_reserved[prefab.name].Add(obj);
+		}
+	}
+
+	/// <summary>インスタンスを借りる(Borrow)</summary>
+	/// <param name="prefab">借りたいインスタンスの元プレハブ</param>
+	/// <returns>インスタンス</returns>
+	public static GameObject Borrow(GameObject prefab)
+	{
+		CheckInitialize();
+
+		if (!_reserved.ContainsKey(prefab.name))
+		{
+			Debug.LogWarningFormat("Object not reserved! name:{0}", prefab.name);
+			return null;
+		}
+
+		//待機中のオブジェクトを探して返す
+		foreach (var item in _reserved[prefab.name])
+		{
+			if (!item.activeSelf)
+			{
+				item.SetActive(true);
+				return item;
+			}
+		}
+
+		//全て使用中ならば新たに確保する
+		Reserve(prefab, 1);
+		return Borrow(prefab);
+	}
+
+	/// <summary>インスタンスを返済する</summary>
+	/// <param name="instance">返済するインスタンス</param>
+	public static void Repay(GameObject instance)
+	{
+		if (_poolRoot.IsChild(instance))
+			Debug.LogWarningFormat("{0} isn't child of [ObjectPool]", instance.name);
+		else
+		{
+			instance.BroadcastMessage("OnRepay", SendMessageOptions.DontRequireReceiver);
+			instance.SetActive(false);
+		}
+	}
+}
---
+++ BlockKuzushi/Assets/MyAssets/ObjectPool/ObjectPoolProfile.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CreateAssetMenu(fileName ="NewProfile")]
+public class ObjectPoolProfile : ScriptableObject
+{
+	[System.Serializable]
+	public class ObjectProfile
+	{
+		public Texture2D _icon;
+		public GameObject _prefab;
+		public int _reserveAmount;
+
+#if UNITY_EDITOR
+		[CustomPropertyDrawer(typeof(ObjectProfile))]
+		public class ObjectProfileDrawer : PropertyDrawer
+		{
+			public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+			{
+				EditorGUI.BeginChangeCheck();
+				EditorGUI.BeginProperty(position, label, property);
+
+				var prefabProp = property.FindPropertyRelative("_prefab");
+				var amountProp = property.FindPropertyRelative("_reserveAmount");
+				var icon = (Texture2D)property.FindPropertyRelative("_icon").objectReferenceValue;
+
+				var iconRect = new Rect(position)
+				{
+					width = 32,
+					height = 32
+				};
+				var prefabRect = new Rect(position)
+				{
+					x = position.x + iconRect.width,
+					width=position.width-iconRect.width,
+					height = position.height * 0.5f
+				};
+				var amountRect = new Rect(prefabRect)
+				{
+					x = position.x + iconRect.width,
+					width = prefabRect.width,
+					y = prefabRect.y + prefabRect.height
+				};
+
+				if (icon != null)
+					EditorGUI.DrawTextureTransparent(iconRect, icon);
+				else
+					EditorGUI.LabelField(iconRect, "NULL");
+				prefabProp.objectReferenceValue = EditorGUI.ObjectField(prefabRect,"Prefab", prefabProp.objectReferenceValue, typeof(GameObject), false);
+				EditorGUI.BeginDisabledGroup(prefabProp.objectReferenceValue == null);
+				EditorGUI.PropertyField(amountRect, amountProp);
+				EditorGUI.EndDisabledGroup();
+
+				EditorGUI.EndProperty();
+				if(EditorGUI.EndChangeCheck())
+				{
+					if (prefabProp.objectReferenceValue != null)
+						property.FindPropertyRelative("_icon").objectReferenceValue = AssetPreview.GetMiniThumbnail(prefabProp.objectReferenceValue);
+					else
+						property.FindPropertyRelative("_icon").objectReferenceValue = null;
+				}
+			}
+
+			public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+			{
+				return base.GetPropertyHeight(property, label) * 2f;
+			}
+		}
+#endif
+	}
+
+	static List<ObjectPoolProfile> _entities = new List<ObjectPoolProfile>();
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+	static void ReserveOnLoad()
+	{
+		foreach(var entity in _entities)
+		{
+			foreach (var prof in entity._profiles)
+				ObjectPool.Reserve(prof._prefab, prof._reserveAmount);
+		}
+	}
+
+
+	[SerializeField]
+	List<ObjectProfile> _profiles = new List<ObjectProfile>();
+
+	void OnEnable()
+	{
+		_entities.Add(this);	
+	}
+
+	public void AddProfile()
+	{
+		_profiles.Add(new ObjectProfile());
+	}
+
+#if UNITY_EDITOR
+	[CustomEditor(typeof(ObjectPoolProfile))]
+	public class ObjectPoolProfileInspector:Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			var self = target as ObjectPoolProfile;
+			serializedObject.Update();
+
+			//追加ボタン
+			if (GUILayout.Button("Add Profile"))
+				self.AddProfile();
+
+			//profile表示
+			var remIndexes = new List<int>();
+			var profiles = serializedObject.FindProperty("_profiles");
+			for (var i = 0; i < profiles.arraySize; i++)
+			{
+				var prof = profiles.GetArrayElementAtIndex(i);
+
+				EditorGUILayout.BeginHorizontal("box");
+				EditorGUILayout.PropertyField(prof);
+				MyGUIUtil.BeginBackgroundColorChange(Color.red);
+				if (GUILayout.Button("X", GUILayout.Width(16), GUILayout.Height(32)))
+					remIndexes.Add(i);
+				MyGUIUtil.EndBackgroundColorChange();
+				EditorGUILayout.EndHorizontal();
+			}
+
+			foreach (var index in remIndexes)
+				profiles.DeleteArrayElementAtIndex(index);
+
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+#endif
+}
